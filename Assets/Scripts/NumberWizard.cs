d141becfc0770ddf89fd9a286690cd90f500593f
cctron <ajcivicogonzalez@gmail.com>
2021-07-31 23:25:52 +0200

    Add Random Range

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -16,26 +16,24 @@ public class NumberWizard : MonoBehaviour
 
     void StartGame()
     {
-       
-        guess = (max + min) / 2;
-        guessText.text = guess.ToString();
-        max++;
+        NextGuess();
     }
 
     public void OnPressHigher()
     {
-        min = guess;
+        min = guess + 1;
         NextGuess();
     }
 
     public void OnPressLower()
     {
-        max = guess;
+        max = guess - 1;
         NextGuess();
     }
 
     void NextGuess()
     {
-        guess = (max + min) / 2;
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
     }
 }
